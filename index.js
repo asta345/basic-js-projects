fdcb6585f3d2639ebd04394a2182ae5272414fe3
asta345 <81383112+asta345@users.noreply.github.com>
2021-08-10 17:54:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+/smooth scroll for website/
+/I am writing for the navbar smooth scroll/
+  var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+ for(var i=0; i<navMenuAnchorTags.length; i++){
+   navMenuAnchorTags[i].addEventListener('click',function(event){
+ event.preventDefault();
+     var targetSectionID =this.textContent.trim().toLowerCase(); 
+     var targetSection = document.getElementById(targetSectionID);
+     var interval = setInterval(function(){
+        var targetSectionCoordinates = targetSection.getBoundingClient();
+        if(targetSectionCoordinates.top <=0){
+          if(targetSectionCoordinates.top <=0){
+            clearInterval(interval);
+          }
+       window.scrollBy(0,50);
+        },20);
+   });
+ }
+
