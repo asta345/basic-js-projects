013fcc1d14cd55e1e9446bf480174502ce706fce
asta345 <81383112+asta345@users.noreply.github.com>
2021-08-10 18:07:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 /smooth scroll for website/
 /I am writing for the navbar smooth scroll/
+  var interval;
   var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
  for(var i=0; i<navMenuAnchorTags.length; i++){
    navMenuAnchorTags[i].addEventListener('click',function(event){
  event.preventDefault();
      var targetSectionID =this.textContent.trim().toLowerCase(); 
      var targetSection = document.getElementById(targetSectionID);
-     var interval = setInterval(function(){
-        var targetSectionCoordinates = targetSection.getBoundingClient();
+     var interval = setInterval(scrollVertically ,20,targetSection);
+      
+   });
+ }
+/SCROLL VERTCIALLY/
+function scrollvertcially(targetSection){
+    var targetSectionCoordinates = targetSection.getBoundingClient();
        
           if(targetSectionCoordinates.top <=0){
             clearInterval(interval);
+            return ;
           }
        window.scrollBy(0,50);
         },20);
-   });
- }
-
+                                         
